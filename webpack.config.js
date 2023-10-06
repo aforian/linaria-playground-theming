9f04d11cadc95213fb89647050c223eeed4ce527
Alex Ian <alexian853@gmail.com>
2023-10-06 12:23:44 +0800

    chore: handle build loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const dev = process.env.NODE_ENV !== 'production';
 
@@ -10,28 +11,56 @@ module.exports = {
   entry: {
     app: './src/App',
   },
+  resolve: {
+    fallback: {
+      "url": false
+    } 
+  },
+  devServer: {
+    historyApiFallback: true,
+    static: {
+      directory: path.join(__dirname, 'public')
+    },
+    compress: true,
+    port: 8080,
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/dist/',
     filename: '[name].bundle.js',
   },
-  optimization: { noEmitOnErrors: true },
+  optimization: {
+    noEmitOnErrors: true,
+    nodeEnv: false,
+  },
   plugins: [
     new webpack.DefinePlugin({
       'process.env': { NODE_ENV: JSON.stringify(process.env.NODE_ENV) },
     }),
-    new MiniCssExtractPlugin({ filename: 'styles.css' }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+    }),
+    new CleanWebpackPlugin(),
   ],
   module: {
     rules: [
+      {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: "javascript/auto",
+      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: [
           { loader: 'babel-loader' },
           {
-            loader: 'linaria/loader',
-            options: { sourceMap: dev },
+            loader: '@linaria/webpack-loader',
+            options: {
+              sourceMap: dev,
+              // displayName: true, // default: false
+              // variableNameConfig: "dashes", // default: "var"
+            },
           },
         ],
       },
@@ -51,12 +80,5 @@ module.exports = {
         use: [{ loader: 'file-loader' }],
       },
     ],
-  },
-  devServer: {
-    historyApiFallback: true,
-    publicPath: '/dist/',
-    contentBase: path.join(__dirname, 'public'),
-    compress: true,
-    port: 8080,
   },
 };
